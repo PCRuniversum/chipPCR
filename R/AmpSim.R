d5128523825cf4e79c1de0983b5a282498555039
devSJR <devSJR@users.noreply.github.com>
2014-05-04 14:28:06 +0200

    Update AmpSim.R
    style improved and comments added

--- R/AmpSim.R
+++ R/AmpSim.R
@@ -1,7 +1,6 @@
 AmpSim <- function(cyc = 1:35, b.eff = -25, bl = 0.05, ampl = 1, 
 		   Cq = 20, noise = FALSE, nnl = 0.025, 
-		   nnl.method = "constant")
-{
+		   nnl.method = "constant") {
 #   tmp.warn <- getOption("warn")
 #   options(warn = -1)
   # Test if x and y exist and have identical lengths.
@@ -13,29 +12,39 @@ AmpSim <- function(cyc = 1:35, b.eff = -25, bl = 0.05, ampl = 1,
     stop("The Cq value must larger than 1.")
   if (nnl < 0 || nnl > 0.1) 
     stop("nnl must be within 0 and 0.1.")
-   
-  fluo <- bl + (ampl - bl)/(1 + exp(b.eff * (log(cyc) - log(Cq))))
-  
-  if (noise) {
-  mean.noise <- mean(fluo) * nnl
-  sd.noise <- sd(fluo) * nnl
+
+# Define the model used to simulate the ammplification curve
+# based on a 5-parameter sigmoidal function
+
+  fluo <- bl + (ampl - bl) / (1 + exp(b.eff * (log(cyc) - log(Cq))))
+
+# Decide if noise is added and how noise is added to the 
+# simulated curve
+# The noise is simulated by the rnorm function and used
+# defined values
+
+ if (noise) {
+ 	mean.noise <- mean(fluo) * nnl
+  	sd.noise <- sd(fluo) * nnl
   if (nnl.method == "increase") {
-		NOISE <- sort(rnorm(length(fluo), 
-				mean = mean.noise, 
-				sd = sd.noise))
-		}
+	NOISE <- sort(rnorm(length(fluo), 
+	mean = mean.noise, 
+	sd = sd.noise))
+  }
   if (nnl.method == "decrease") {
-		NOISE <- sort(rnorm(length(fluo), 
-		mean = mean.noise, 
-		sd = sd.noise), decreasing = TRUE)
-		}
+	NOISE <- sort(rnorm(length(fluo), 
+			mean = mean.noise, 
+			d = sd.noise), decreasing = TRUE)
+  }
   if (nnl.method == "constant") {
-		NOISE <- rnorm(length(fluo), 
-				mean = mean.noise, 
-				sd = sd.noise)
-		}
-  fluo <- fluo + NOISE
-  res <- data.frame(cyc, fluo)
+	NOISE <- rnorm(length(fluo), 
+			mean = mean.noise, 
+			sd = sd.noise)
+  }
+    
+# Add the noise the the simulated curve and combine the output
+    fluo <- fluo + NOISE
+    res <- data.frame(cyc, fluo)
   } else (res <- data.frame(cyc, fluo))
 #   options(warn = tmp.warn)
   res
