c13ab82d23ec75029c7ebf1913d9d83b7385bb62
michbur <michalburdukiewicz@gmail.com>
2014-05-07 22:26:30 +0200

    Added inder to bg.max

--- R/bg.max.R
+++ R/bg.max.R
@@ -1,11 +1,11 @@
-bg.max <- function (x, y, bg.corr = 1.3, bg.start = 3) {
+bg.max <- function (x, y, bg.corr = 1.3, bg.start = 3, inder.approx = TRUE) {
   stop("Wrong classes of 'x'", call. = TRUE, domain = NA)
 }
 
 setGeneric("bg.max")
 
 setMethod("bg.max", signature(x = "numeric", y = "numeric"), 
-          function(x, y, bg.corr = 1.3, bg.start = 3) {
+          function(x, y, bg.corr = 1.3, bg.start = 3, inder.approx = TRUE) {
             # Test if x and y have identical lengths.
             if (is.null(y)) 
               stop("Enter ordinate value")
@@ -13,25 +13,25 @@ setMethod("bg.max", signature(x = "numeric", y = "numeric"),
             if (length(x) != length(y)) 
               stop("Use abscissa and ordinate data with same number of elements")
             
-            calc.bg(x, y, bg.corr, bg.start)
+            calc.bg(x, y, bg.corr, bg.start, inder.approx)
           })
 
 setMethod("bg.max", signature(x = "data.frame", y="missing"), 
-          function(x, y, bg.corr = 1.3, bg.start = 3) { 
+          function(x, y, bg.corr = 1.3, bg.start = 3, inder.approx = TRUE) { 
             if (ncol(x) != 2) 
               stop("'x' must have two columns.")
-            calc.bg(x[, 1], x[, 2], bg.corr, bg.start)
+            calc.bg(x[, 1], x[, 2], bg.corr, bg.start, inder.approx)
           })
 
 setMethod("bg.max", signature(x = "matrix", y = "missing"), 
-          function(x, y, bg.corr = 1.3, bg.start = 3) { 
+          function(x, y, bg.corr = 1.3, bg.start = 3, inder.approx = TRUE) { 
             if (ncol(x) != 2) 
               stop("'x' must have two columns.")
-            calc.bg(x[, 1], x[, 2], bg.corr, bg.start)
+            calc.bg(x[, 1], x[, 2], bg.corr, bg.start, inder.approx)
           })
 
 #workhorse function
-calc.bg <- function(x, y, bg.corr, bg.start) {
+calc.bg <- function(x, y, bg.corr, bg.start, inder.approx) {
   input <- data.frame(cyc = x, fluo = y)
   
   # Test if bg.corr is within a meaningful range.
@@ -52,26 +52,34 @@ calc.bg <- function(x, y, bg.corr, bg.start) {
   # transistion phase from linear to exponential
   yval.d <- supsmu(x, y, span = 0.09)$y
   
+  if (inder.approx) {
+    der <- inder(x, yval.d)
+    deltax <- der[["x"]]
+    deltax1 <- der[["x"]]
+    delta <- der[["1st_der"]]
+    delta1 <- der[["2nd_der"]]
+  } else {
   # Form the derivatives of the smoothed data.
   # The maximum and the minimum of the seconde derivative
   # are the starting points to define the approximate 
   # start and the end of the exponential phase
-  delta <- vector()
-  deltax <- vector()
-  for (i in 1L:(length(yval.d) - 1)) {
-    delta.t <- yval.d[i + 1] - yval.d[i]
-    delta <- c(delta, delta.t)
-    delta.tx <- (x[i + 1] + x[i]) / 2
-    deltax <- c(deltax, delta.tx)
-  }
-  
-  delta1 <- vector() 
-  deltax1 <- vector() 
-  for (i in 1L:(length(delta) - 1)) { 
-    delta.t <- delta[i + 1] - delta[i] 
-    delta1 <- c(delta1, delta.t) 
-    delta.tx <- (deltax[i + 1] + deltax[i]) / 2 
-    deltax1 <- c(deltax1, delta.tx)
+    delta <- vector()
+    deltax <- vector()
+    for (i in 1L:(length(yval.d) - 1)) {
+      delta.t <- yval.d[i + 1] - yval.d[i]
+      delta <- c(delta, delta.t)
+      delta.tx <- (x[i + 1] + x[i]) / 2
+      deltax <- c(deltax, delta.tx)
+    }
+    
+    delta1 <- vector() 
+    deltax1 <- vector() 
+    for (i in 1L:(length(delta) - 1)) { 
+      delta.t <- delta[i + 1] - delta[i] 
+      delta1 <- c(delta1, delta.t) 
+      delta.tx <- (deltax[i + 1] + deltax[i]) / 2 
+      deltax1 <- c(deltax1, delta.tx)
+    }
   }
   
   d <- data.frame(deltax, delta) 
