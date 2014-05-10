b1eefb93a8d021384e6fa6fed6861e1f64e00d37
michbur <michalburdukiewicz@gmail.com>
2014-05-10 14:48:35 +0200

    Changes to bg.max

--- R/bg.max.R
+++ R/bg.max.R
@@ -1,5 +1,5 @@
 bg.max <- function(x, y, bg.corr = 1.3, bg.start = 3, 
-		    inder.approx = TRUE) {
+                   inder.approx = TRUE) {
   stop("Wrong classes of 'x'", call. = TRUE, domain = NA)
 }
 
@@ -7,7 +7,7 @@ setGeneric("bg.max")
 
 setMethod("bg.max", signature(x = "numeric", y = "numeric"), 
           function(x, y, bg.corr = 1.3, bg.start = 3, 
-		    inder.approx = TRUE) {
+                   inder.approx = TRUE) {
             # Test if x and y have identical lengths.
             if (is.null(y)) 
               stop("Enter ordinate value")
@@ -21,7 +21,7 @@ setMethod("bg.max", signature(x = "numeric", y = "numeric"),
 
 setMethod("bg.max", signature(x = "data.frame", y="missing"), 
           function(x, y, bg.corr = 1.3, bg.start = 3, 
-          inder.approx = TRUE) { 
+                   inder.approx = TRUE) { 
             if (ncol(x) != 2) 
               stop("'x' must have two columns.")
             calc.bg(x[, 1], x[, 2], bg.corr, bg.start, inder.approx)
@@ -29,7 +29,7 @@ setMethod("bg.max", signature(x = "data.frame", y="missing"),
 
 setMethod("bg.max", signature(x = "matrix", y = "missing"), 
           function(x, y, bg.corr = 1.3, bg.start = 3, 
-          inder.approx = TRUE) { 
+                   inder.approx = TRUE) { 
             if (ncol(x) != 2) 
               stop("'x' must have two columns.")
             calc.bg(x[, 1], x[, 2], bg.corr, bg.start, inder.approx)
@@ -52,64 +52,37 @@ calc.bg <- function(x, y, bg.corr, bg.start, inder.approx) {
   
   y <- fixNA(x, y, spline = TRUE)
   
-  # Use Friedman's SuperSmoother to smooth the data
-  # The smoothed data have less steep slopes in the 
-  # transistion phase from linear to exponential
-  yval.d <- supsmu(x, y, span = 0.09)$y
-  
-  if (inder.approx) {
-    der <- slot(inder(x, yval.d), ".Data")
-    deltax <- der[, "x"]
-    deltax1 <- der[, "x"]
-    delta <- der[, "d1y"]
-    delta1 <- der[, "d2y"]
-  } else {
   # Form the derivatives of the smoothed data.
   # The maximum and the minimum of the seconde derivative
   # are the starting points to define the approximate 
   # start and the end of the exponential phase
-    delta <- vector()
-    deltax <- vector()
-    for (i in 1L:(length(yval.d) - 1)) {
-      delta.t <- yval.d[i + 1] - yval.d[i]
-      delta <- c(delta, delta.t)
-      delta.tx <- (x[i + 1] + x[i]) / 2
-      deltax <- c(deltax, delta.tx)
-    }
-    
-    delta1 <- vector() 
-    deltax1 <- vector() 
-    for (i in 1L:(length(delta) - 1)) { 
-      delta.t <- delta[i + 1] - delta[i] 
-      delta1 <- c(delta1, delta.t) 
-      delta.tx <- (deltax[i + 1] + deltax[i]) / 2 
-      deltax1 <- c(deltax1, delta.tx)
-    }
+  
+  if (inder.approx) {
+    der <- inder(x, y)
+  } else {
+    #Now what?
   }
   
-  d <- data.frame(deltax, delta) 
-  d1 <- data.frame(deltax1, delta1)
+  vals <- summary(der, print = FALSE)
   
-  bg.stop <- trunc(d1[which(d1[, 2] == max(d1[, 2])), 1] - 
-                     bg.corr * (d1[which(d1[, 2] == min(d1[, 2])), 1] - 
-                                  d1[which(d1[, 2] == max(d1[, 2])), 1]), 0)
+  bg.stop <- trunc(vals[["SDM"]] - bg.corr * (vals[["SDm"]] - vals[["SDM"]]), 0)
   
   
-  amp.stop <- trunc(d1[which(d1[, 2] == min(d1[,  2])), 1] + 
-                      bg.corr * (d1[which(d1[, 2] == min(d1[, 2])), 1] - 
-                                   d[which(d[, 2] == max(d[, 2])), 1]), 0) 
+  amp.stop <- trunc(vals[["SDm"]] + bg.corr * (vals[["SDm"]] - vals[["SDM"]]), 0) 
   
   # Perform error handling on the the estimated start and end 
   # of the amplification process. Used hard coded values to prevent
   # to early or to late bg.start or bg.stop values.
-  if (is.na(bg.stop)) {bg.stop <- round(length(yval.d) * 0.8)}
-  if (bg.stop <= 9) {bg.stop <- 10}
-  if ((bg.stop >= length(yval.d) * 0.6) || (is.na(bg.stop))) {
-    bg.stop <- round(length(yval.d) * 0.6)
-  }
+  if (is.na(bg.stop)) 
+    bg.stop <- round(length(y) * 0.8)
+  if (bg.stop <= 9) 
+    bg.stop <- 10
+  if ((bg.stop >= length(y) * 0.6) || (is.na(bg.stop))) 
+    bg.stop <- round(length(y) * 0.6)
+  
   #threshold bg.max
   th.bg <- median(y[c(bg.stop - 1, bg.stop, bg.stop + 1)])  
-  th <- quantile(y, 0.4)	#threshold qPCR background
+  th <- quantile(y, 0.4)  #threshold qPCR background
   if (th.bg >= th) {bg.stop <- 10}
   fluo <- y[bg.stop]
   
@@ -117,9 +90,98 @@ calc.bg <- function(x, y, bg.corr, bg.start, inder.approx) {
   if (bg.stop <= bg.start) 
     stop("Start of background must be less than maximal 
 		  background value.")
-  new("bg", d = d, d1 = d1, delta = delta, delta1 = delta1, 
+  new("bg", .Data = slot(der, ".Data"),
       bg.start = bg.start, bg.stop = bg.stop, 
-      bg.corr = bg.corr, fluo = fluo, amp.stop = amp.stop,
-      raw.data = input)
-  
+      bg.corr = bg.corr, fluo = fluo, amp.stop = amp.stop)
 }
+
+
+#old version temporarily kept here
+# calc.bg <- function(x, y, bg.corr, bg.start, inder.approx) {
+#   input <- data.frame(cyc = x, fluo = y)
+#   
+#   # Test if bg.corr is within a meaningful range.
+#   if (bg.corr < 1 || bg.corr > 8) 
+#     stop("bg.corr must be within 1 and 8.")
+#   
+#   # Test if bg.corr is within a meaningful range.
+#   if (bg.start < 0 || bg.start > length(x)) 
+#     stop("bg.start must be within 0 and the number of x values.")
+#   
+#   # Remove missing calues form y by using the fixNA function 
+#   # with the cubic spline method
+#   
+#   y <- fixNA(x, y, spline = TRUE)
+#   
+#   # Use Friedman's SuperSmoother to smooth the data
+#   # The smoothed data have less steep slopes in the 
+#   # transistion phase from linear to exponential
+# 
+#   yval.d <- supsmu(x, y, span = 0.09)$y
+#   
+#   if (inder.approx) {
+#     der <- slot(inder(x, yval.d), ".Data")
+#     deltax <- der[, "x"]
+#     deltax1 <- der[, "x"]
+#     delta <- der[, "d1y"]
+#     delta1 <- der[, "d2y"]
+#   } else {
+#   # Form the derivatives of the smoothed data.
+#   # The maximum and the minimum of the seconde derivative
+#   # are the starting points to define the approximate 
+#   # start and the end of the exponential phase
+#     delta <- vector()
+#     deltax <- vector()
+#     for (i in 1L:(length(yval.d) - 1)) {
+#       delta.t <- yval.d[i + 1] - yval.d[i]
+#       delta <- c(delta, delta.t)
+#       delta.tx <- (x[i + 1] + x[i]) / 2
+#       deltax <- c(deltax, delta.tx)
+#     }
+#     
+#     delta1 <- vector() 
+#     deltax1 <- vector() 
+#     for (i in 1L:(length(delta) - 1)) { 
+#       delta.t <- delta[i + 1] - delta[i] 
+#       delta1 <- c(delta1, delta.t) 
+#       delta.tx <- (deltax[i + 1] + deltax[i]) / 2 
+#       deltax1 <- c(deltax1, delta.tx)
+#     }
+#   }
+#   
+#   d <- data.frame(deltax, delta) 
+#   d1 <- data.frame(deltax1, delta1)
+#   
+#   bg.stop <- trunc(d1[which(d1[, 2] == max(d1[, 2])), 1] - 
+#                      bg.corr * (d1[which(d1[, 2] == min(d1[, 2])), 1] - 
+#                                   d1[which(d1[, 2] == max(d1[, 2])), 1]), 0)
+#   
+#   
+#   amp.stop <- trunc(d1[which(d1[, 2] == min(d1[,  2])), 1] + 
+#                       bg.corr * (d1[which(d1[, 2] == min(d1[, 2])), 1] - 
+#                                    d[which(d[, 2] == max(d[, 2])), 1]), 0) 
+#   
+#   # Perform error handling on the the estimated start and end 
+#   # of the amplification process. Used hard coded values to prevent
+#   # to early or to late bg.start or bg.stop values.
+#   if (is.na(bg.stop)) {bg.stop <- round(length(yval.d) * 0.8)}
+#   if (bg.stop <= 9) {bg.stop <- 10}
+#   if ((bg.stop >= length(yval.d) * 0.6) || (is.na(bg.stop))) {
+#     bg.stop <- round(length(yval.d) * 0.6)
+#   }
+#   #threshold bg.max
+#   th.bg <- median(y[c(bg.stop - 1, bg.stop, bg.stop + 1)])  
+#   th <- quantile(y, 0.4)	#threshold qPCR background
+#   if (th.bg >= th) {bg.stop <- 10}
+#   fluo <- y[bg.stop]
+#   
+#   # Test if background range is meaningful.
+#   if (bg.stop <= bg.start) 
+#     stop("Start of background must be less than maximal 
+# 		  background value.")
+#   new("bg", d = d, d1 = d1, delta = delta, delta1 = delta1, 
+#       bg.start = bg.start, bg.stop = bg.stop, 
+#       bg.corr = bg.corr, fluo = fluo, amp.stop = amp.stop,
+#       raw.data = input)
+#   
+# }
--- R/classes.R
+++ R/classes.R
@@ -27,18 +27,83 @@ setMethod("summary", signature(object = "amptest"), function(object) {
   }
 })
 
+#der class ----------------------
+setClass("der", contains = "matrix", representation(.Data = "matrix", 
+                                                    method = "character"))
+
+
+setMethod("summary", signature(object = "der"), function(object, digits = 0, print = TRUE) {
+  data <- slot(object, ".Data")
+  FDM <- data[data[, "d1y"] == max(data[, "d1y"]), "x"] 
+  SDM <- data[data[, "d2y"] == max(data[, "d2y"]), "x"]
+  SDm <- data[data[, "d2y"] == min(data[, "d2y"]), "x"]
+  SDC <- sqrt(SDM * SDm)
+  if (print) {
+    cat(paste0("Smoothing method: ", slot(object, "method")))
+    cat(paste0("\nFirst derivative maximum: ", round(FDM, digits = digits)))
+    cat(paste0("\nSecond derivative maximum: ", round(SDM, digits = digits)))
+    cat(paste0("\nSecond derivative minimum: ", round(SDm, digits = digits)))
+    cat(paste0("\nSecond derivative center: ", round(SDC, digits = digits)))
+  }
+  res <- c(FDM, SDM, SDm, SDC)
+  names(res) <- c("FDM", "SDM", "SDm", "SDC")
+  invisible(res)
+})
+
+setMethod("show", signature(object = "der"), function(object) {
+  print(slot(object, ".Data"))
+})
+
+setMethod("plot", signature(x = "der"), function(x, what = 1:3, add = FALSE, indicators = TRUE,
+                                                 legend = TRUE,
+                                                 plot.colors = c("black", "red", "blue"), 
+                                                 ...) {
+  if (!all(what %in% 1:3)) 
+    stop("'what' must contain values from set: 1, 2, 3.")
+  
+  if (length(unique(what)) != length(what)) 
+    stop("'what' must contain unique values.")
+  
+  if (length(plot.colors) != 3) 
+    stop("'plot.colors' must contain three colors.")
+  
+  
+  
+  #smallest and biggest fluorescence values
+  ylims <- range(sapply(what + 1, function(i) {
+    x[, i]
+  }))
+  
+  if(!add) {
+    plot(x = range(x[, 1]),  y = ylims, xlab = "Cycles", 
+         ylab = "Fluorescence", cex = 0, ...)
+    
+    #     if (indicators) {
+    #       abline(v = slot(x, "bg.start"))
+    #       text(slot(x, "bg.start"), 0.2, "Background start", pos = 4)
+    #       abline(v = slot(x, "bg.stop"), col = "blue")
+    #       text(slot(x, "bg.stop"), 0.25, "Background stop", pos = 4, col = "blue")
+    #       abline(v = slot(x, "amp.stop"), col = "green")
+    #       text(slot(x, "amp.stop"), 0.3, "Plateau transition", pos = 4, col = "green")
+    #     }
+  }
+  for (i in what)
+    points(x[, c(1, i + 1)], col = plot.colors[i], type = "b", pch = 20)
+  
+  
+  if (legend)
+    legend("topleft", c("Raw data", "First derivative", "Second derivative")[what], 
+           pch = rep(20,3), col = plot.colors)
+})
+
 
 #bg class, bg.max function -------------------------------
-setClass("bg", representation(d = "data.frame", 
-                              d1 = "data.frame",
-                              delta = "numeric",
-                              delta1 = "numeric",
+setClass("bg", contains = "der", representation(.Data = "matrix", 
                               bg.start = "numeric",
                               bg.stop = "numeric",
                               bg.corr = "numeric",
                               fluo = "numeric",
-                              amp.stop = "numeric",
-                              raw.data = "data.frame"))
+                              amp.stop = "numeric"))
 
 
 setMethod("summary", signature(object = "bg"), function(object, print = TRUE) {
@@ -103,71 +168,4 @@ setMethod("plot", signature(x = "bg"), function(x, what = 1:3, add = FALSE, indi
 
 
 
-#der class ----------------------
-setClass("der", contains = "matrix", representation(.Data = "matrix", 
-                               method = "character"))
 
-
-setMethod("summary", signature(object = "der"), function(object, digits = 0, print = TRUE) {
-  data <- slot(object, ".Data")
-  FDM <- data[data[, "d1y"] == max(data[, "d1y"]), "x"] 
-  SDM <- data[data[, "d2y"] == max(data[, "d2y"]), "x"]
-  SDm <- data[data[, "d2y"] == min(data[, "d2y"]), "x"]
-  SDC <- sqrt(SDM * SDm)
-  if (print) {
-    cat(paste0("Smoothing method: ", slot(object, "method")))
-    cat(paste0("\nFirst derivative maximum: ", round(FDM, digits = digits)))
-    cat(paste0("\nSecond derivative maximum: ", round(SDM, digits = digits)))
-    cat(paste0("\nSecond derivative minimum: ", round(SDm, digits = digits)))
-    cat(paste0("\nSecond derivative center: ", round(SDC, digits = digits)))
-  }
-  res <- c(FDM, SDM, SDm, SDC)
-  names(res) <- c("FDM", "SDM", "SDm", "SDC")
-  invisible(res)
-})
-
-setMethod("show", signature(object = "der"), function(object) {
-  print(slot(object, ".Data"))
-})
-
-setMethod("plot", signature(x = "der"), function(x, what = 1:3, add = FALSE, indicators = TRUE,
-                                                 legend = TRUE,
-                                                 plot.colors = c("black", "red", "blue"), 
-                                                 ...) {
-  if (!all(what %in% 1:3)) 
-    stop("'what' must contain values from set: 1, 2, 3.")
-  
-  if (length(unique(what)) != length(what)) 
-    stop("'what' must contain unique values.")
-  
-  if (length(plot.colors) != 3) 
-    stop("'plot.colors' must contain three colors.")
-  
-
-  
-  #smallest and biggest fluorescence values
-  ylims <- range(sapply(what + 1, function(i) {
-    x[, i]
-  }))
-  
-  if(!add) {
-    plot(x = range(x[, 1]),  y = ylims, xlab = "Cycles", 
-         ylab = "Fluorescence", cex = 0, ...)
-    
-#     if (indicators) {
-#       abline(v = slot(x, "bg.start"))
-#       text(slot(x, "bg.start"), 0.2, "Background start", pos = 4)
-#       abline(v = slot(x, "bg.stop"), col = "blue")
-#       text(slot(x, "bg.stop"), 0.25, "Background stop", pos = 4, col = "blue")
-#       abline(v = slot(x, "amp.stop"), col = "green")
-#       text(slot(x, "amp.stop"), 0.3, "Plateau transition", pos = 4, col = "green")
-#     }
-  }
-  for (i in what)
-    points(x[, c(1, i + 1)], col = plot.colors[i], type = "b", pch = 20)
-  
-  
-  if (legend)
-    legend("topleft", c("Raw data", "First derivative", "Second derivative")[what], 
-           pch = rep(20,3), col = plot.colors)
-})
