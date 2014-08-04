7260579bd9d2b9b7dd9f3c14bcd7a754e09b9f93
devSJR <stefan.roediger@hs-lausitz.de>
2014-08-04 10:13:42 +0200

    optimized th.cyc function

--- R/th.cyc.R
+++ R/th.cyc.R
@@ -8,30 +8,50 @@ xy <- data.frame(x = x, y = y)
 # Determin type of threshold calculation
 if (auto) r <- quantile(y[1:10], 0.1) + 3 * mad(y[1:10]) else r <- r
 
-# Fetch the neighbours of the cycle and fluorescence values at the threshold
-# fluorescence.
-xy.out <- rbind(tail(xy[xy[, 2] <= r, ], 2),
-		head(xy[xy[, 2] >= r, ], 2)
-		)
+# Helper function to determine the number of neighbours for the regression
+th.est <- function(xy, r = r, n) {
+    # Fetch the neighbours of the cycle and fluorescence values at the threshold
+    # fluorescence.
 
-# Determine with a quaratic polynome the equantion for the neighbours of the 
-# cycle and fluorescence values at the threshold fluorescence.
+    xy.out <- rbind(tail(xy[xy[, 2] <= r, ], n),
+		    head(xy[xy[, 2] >= r, ], n)
+		    )
 
-xy.lm <- lm(xy.out[, 2] ~ xy.out[, 1] + I(xy.out[, 1]^2))
+    # Determine with a quaratic polynome the equantion for the neighbours of the 
+    # cycle and fluorescence values at the threshold fluorescence.
 
-# Extract the coefficients of the regression.
-xy.coeff <- coefficients(xy.lm)
-c <- xy.coeff[1]
-b <- xy.coeff[2]
-a <- xy.coeff[3]
+    xy.lm <- lm(xy.out[, 2] ~ xy.out[, 1] + I(xy.out[, 1]^2))
 
-# Calculate the exact Ct value at the user defined fluorescence threshold.
-x.cal <- (-b/a)/2 + sqrt(((-b/a)/2)^2 - (c - r)/a)
+    # summary of statistical values of the fit
+    out <- list(summary = summary(xy.lm), values = xy.out)
+}
+    # Actually used number of neighbours
+    n <- seq(2,8,1)
+    
+    # List of all regression results for all tested regressions with different 
+    # numbers of neighbours
+    res.th.est <- lapply(n, function(n) {th.est(xy, r = r, n)})
+        
+    # Results of the selection criterium R suquared
+    res.r.squ <- data.frame(r.sqared = sapply(1:length(n), 
+			    function(i) res.th.est[[i]][[1]]$r.squared), 
+			    n = n)
+    
+    # Result of the optimal regression
+    xy.sum <- res.th.est[[which(res.r.squ[, 1] == max(res.r.squ[, 1]))]]
 
+    # Extract the coefficients of the regression.
+    a <- xy.sum[[1]][["coefficients"]][3,1]
+    b <- xy.sum[[1]][["coefficients"]][2,1]
+    c <- xy.sum[[1]][["coefficients"]][1,1]
+
+    # Calculate the exact Ct value at the user defined fluorescence threshold.
+    x.cal <- (-b/a)/2 + sqrt(((-b/a)/2)^2 - (c - r)/a)
+    
 # Create the output fot the exact Ct value, the regression and the neighbours 
 # of the cycle and fluorescence values at the threshold fluorescence.
 
-out <- list(Results = rbind(cyc.th = x.cal, atFluo = r), fit = xy.lm, 
-	    Input = xy.out)
+out <- list(Results = rbind(cyc.th = x.cal, atFluo = r), stats = xy.sum, 
+	    Input = xy.sum$values)
 out
 }
