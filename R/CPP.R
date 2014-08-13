b92b75417d7f600c819a30ef1e6dd19c494aeb7d
michbur <michalburdukiewicz@gmail.com>
2014-08-13 11:22:03 +0200

    Added lm.coefs

--- R/CPP.R
+++ R/CPP.R
@@ -1,28 +1,9 @@
 CPP <- function(x, y, smoother = TRUE, method = "savgol", trans = FALSE, 
-		rob.reg = "lmrob", bg.outliers = FALSE, median = FALSE, 
-		method.norm = "none", qnL = 0.03, amptest = FALSE, manual = FALSE, 
-		nl = 0.08, ...) {
+                method.reg = "lmrob", bg.outliers = FALSE, median = FALSE, 
+                method.norm = "none", qnL = 0.03, amptest = FALSE, manual = FALSE, 
+                nl = 0.08, ...) {
   
   testxy(x, y)
-  
-
-    
-  # Define the method for the linear regression
-  # lmrob (robustbase) uses MM-type estimators, rfit (Rfit)
-  # uses a rank-based estimation model for linear regression,
-  # and lm (stats) an ordinary least squares 
-  
-  method.reg <- tolower(rob.reg)
-  if (grepl(method.reg, "lmrob"))
-    method.reg <- "lmrob"
-  if (grepl(method.reg, "rfit")) 
-    method.reg <- "rfit"
-  if (grepl(method.reg, "least")) 
-    method.reg <- "least"
-  if (grepl(method.reg, "rq")) 
-    method.reg <- "rq"
-  if (!(method.reg %in% c("lmrob", "rfit", "least", "rq")))
-    stop("Invalid regression method chosen.")
   
   # Define the method for the smoothing
   # functionality identical to smoother function
@@ -44,17 +25,10 @@ CPP <- function(x, y, smoother = TRUE, method = "savgol", trans = FALSE,
   if (grepl(method.smooth, "whit2")) 
     method.smooth <- "whit2"
   if (!(method.smooth %in% c("savgol", "lowess", "mova", "smooth", "spline", 
-			     "supsmu", "whit1", "whit2")))
+                             "supsmu", "whit1", "whit2")))
     stop("Invalid smoothing/filter method chosen.")
   
-  lm.fit <- function(y, x, method) {
-    switch(method,
-           lmrob = do.call(function(x, y) lmrob(y ~ x), c(list(x = x, y = y))),
-           rfit = do.call(function(x, y) rfit(y ~ x), c(list(x = x, y = y))),
-           least = do.call(function(x, y) lm(y ~ x), c(list(x = x, y = y))),
-           rq = do.call(function(x, y) rq(y ~ x), c(list(x = x, y = y)))
-    )	
-  }
+  
   
   # Remove missing values from y
   if(any(is.na(y)))
@@ -67,10 +41,10 @@ CPP <- function(x, y, smoother = TRUE, method = "savgol", trans = FALSE,
   # Remove outliers (high and low) from the background range and 
   # substitute with the median
   if (bg.outliers) { 
-    y[c(BG)] 	<- rm.outlier(y[c(BG)], fill = TRUE, 
-                            median = median)
-    y[c(BG)] 	<- rm.outlier(y[c(BG)], opposite = TRUE, 
-                            fill = TRUE, median = median)
+    y[BG] 	<- rm.outlier(y[BG], fill = TRUE, 
+                         median = median)
+    y[BG] 	<- rm.outlier(y[BG], opposite = TRUE, 
+                         fill = TRUE, median = median)
   }
   
   # Invoke smoother to improve data for further calculations
@@ -79,7 +53,7 @@ CPP <- function(x, y, smoother = TRUE, method = "savgol", trans = FALSE,
   
   if (smoother) {
     y <- smoother(x, y, trans = FALSE, bg.outliers = FALSE, 
-			method = method.smooth, CPP = FALSE, ...)
+                  method = method.smooth, CPP = FALSE, ...)
     #some smoothing methods (for example mova) can introduce missing values
     if(any(is.na(y))) {
       y <- fixNA(x, y, spline = TRUE)
@@ -87,36 +61,21 @@ CPP <- function(x, y, smoother = TRUE, method = "savgol", trans = FALSE,
                      method.smooth, ". fixNA() used."))
     }
   } else {
-      y
+    y
   }
   
   # Test if linear correction based on the background range is requested
   # If requested first try a robust linear regression. If robust linear 
   # regression fails try standard lm()
   if (trans) {
-    if (class(try(lm.fit(y[c(BG)], x[c(BG)], method = method.reg), 
-				  silent = TRUE)) == "try-error") { 
-      coefficients <- data.frame(lm.fit(y[c(BG)], x[c(BG)], 
-					  method = "least")[1]) 
-    } else { 
-      lmrob.control <- suppressWarnings(lm.fit(y[c(BG)], x[c(BG)], 
-					  method = method.reg)) 
-      if ((class(lmrob.control) != "try-error") && 
-            (lmrob.control$converged == TRUE)) { 
-        coefficients <- data.frame(lm.fit(y[c(BG)], x[c(BG)], 
-					  method = method.reg)[1]) 
-      } else { 
-        coefficients <- data.frame(lm.fit(y[c(BG)], x[c(BG)], 
-					  method = "least")[1]) 
-      } 
-    }
+    coefficients <- lm.coefs(x[BG], y[BG], method.reg)
     # Apply linear model to the raw data
     y.norm <- y - (coefficients[2, 1] * x + coefficients[1, 1])
     # Subtract the median (based on background range) from the data
-    y.norm <- y.norm - median(y.norm[c(BG)]) 
+    y.norm <- y.norm - median(y.norm[BG]) 
     # Subtract the median (based on background range) from the data 
     # without a linear model
-  } else {y.norm <- y - median(y[c(BG)])}
+  } else {y.norm <- y - median(y[BG])}
   
   # Perform a normalization to a specified quantile value
   y.norm <- normalizer(y = y.norm, method.norm = method.norm, qnL = qnL)
@@ -138,28 +97,29 @@ CPP <- function(x, y, smoother = TRUE, method = "savgol", trans = FALSE,
 
 setGeneric("CPP")
 
-  
+
 setMethod("CPP", signature(x = "data.frame", y="missing"), 
-          function(x, y, smoother = TRUE, trans = FALSE, rob.reg = "lmrob", 
-		   bg.outliers = FALSE, median = FALSE, 
+          function(x, y, smoother = TRUE, trans = FALSE, method.reg = "lmrob", 
+                   bg.outliers = FALSE, median = FALSE, 
                    method.norm = "none", qnL = 0.03, 
                    amptest = FALSE, manual = FALSE, nl = 0.08, ...) { 
             if (ncol(x) != 2) 
               stop("'x' must have two columns.")
-            CPP(x[, 1], x[, 2], trans = trans, bg.outliers = bg.outliers, 
+            CPP(x[, 1], x[, 2], trans = trans, method.reg = "lmrob", ,
+                bg.outliers = bg.outliers, 
                 median = median, method.norm = method.norm, qnL = qnL,
                 amptest = amptest, manual = manual, nl = nl, ...)
           })
 
 setMethod("CPP", signature(x = "matrix", y="missing"), 
-          function(x, y, smoother = TRUE, trans = FALSE, rob.reg = "lmrob", 
-		   bg.outliers = FALSE, median = FALSE, 
+          function(x, y, smoother = TRUE, trans = FALSE, method.reg = "lmrob", 
+                   bg.outliers = FALSE, median = FALSE, 
                    method.norm = "none", qnL = 0.03, 
                    amptest = FALSE, manual = FALSE, nl = 0.08, ...) { 
             if (ncol(x) != 2) 
               stop("'x' must have two columns.")
             CPP(x[, 1], x[, 2], smoother = TRUE, trans = trans, 
-		rob.reg = "lmrob", bg.outliers = bg.outliers, 
+                method.reg = "lmrob", bg.outliers = bg.outliers, 
                 median = median, method.norm = method.norm, qnL = qnL,
                 amptest = amptest, manual = manual, nl = nl, ...)
           })
---
+++ R/lm.coefs.R
@@ -0,0 +1,39 @@
+lm.coefs <- function(x, y, method.reg) {
+  
+  # Define the method for the linear regression
+  # lmrob (robustbase) uses MM-type estimators, rfit (Rfit)
+  # uses a rank-based estimation model for linear regression,
+  # and lm (stats) an ordinary least squares 
+  
+  method.reg <- tolower(method.reg)
+  if (grepl(method.reg, "lmrob"))
+    method.reg <- "lmrob"
+  if (grepl(method.reg, "rfit")) 
+    method.reg <- "rfit"
+  if (grepl(method.reg, "least")) 
+    method.reg <- "least"
+  if (grepl(method.reg, "rq")) 
+    method.reg <- "rq"
+  if (!(method.reg %in% c("lmrob", "rfit", "least", "rq")))
+    stop("Invalid regression method chosen.")
+  
+  lm.fit <- function(y, x, method) {
+    switch(method,
+           lmrob = do.call(function(x, y) lmrob(y ~ x), c(list(x = x, y = y))),
+           rfit = do.call(function(x, y) rfit(y ~ x), c(list(x = x, y = y))),
+           least = do.call(function(x, y) lm(y ~ x), c(list(x = x, y = y))),
+           rq = do.call(function(x, y) rq(y ~ x), c(list(x = x, y = y)))
+    )  
+  }
+  
+  tried.fit <- try(lm.fit(y, x, method = method.reg), 
+                   silent = TRUE)
+  if(is.null(tried.fit[["converged"]]))
+     tried.fit[["converged"]] <- TRUE
+  if (class(tried.fit) != "try-error" && tried.fit[["converged"]] == TRUE) { 
+    coefficients <- data.frame(tried.fit[1])
+  } else { 
+    coefficients <- data.frame(lm.fit(y, x, method = "least")[1]) 
+  } 
+  coefficients
+}
