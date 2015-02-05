f348eb3b31f5c36902bfb77e41012bd596d205a5
michbur <michalburdukiewicz@gmail.com>
2015-02-05 17:45:01 +0100

    th.cyc fixed (thx Konstantin)

--- R/th.cyc.R
+++ R/th.cyc.R
@@ -3,56 +3,56 @@ th.cyc <-
     # Sanity test for input values
     testxy(x, y, length = FALSE)
     # Rearrange data for further processing
- 
+    
     xy <- data.frame(x = x, y = y)
     xy <- xy[!is.na(xy[["x"]]), ]
     
-    
-    # Determin type of threshold calculation
+    # Determine type of threshold calculation
     r <- ifelse(auto, quantile(y[1L:10], 0.1) + 3 * mad(y[1L:10]), r)
     
     # Before runing the analysis, test if signal is indeed larger than the 
     # threshold.
     
     if (quantile(xy[, 2], 0.9) <= r) {
-    	  # TODO: FIX OUTPUT
-	  warning("Maximum of signal lower than threshold (r).")
+      # TODO: FIX OUTPUT
+      stop("Maximum of signal lower than threshold (r).")
     } else {
       # Actually used number of neighbours around the threshold value
       n <- seq(2, 8, 1)
       
       # List of all regression results for all tested regressions with different 
       # numbers of neighbours
-      res.th.est <- lapply(n, function(n) {th.est(xy, r = r, n, linear = linear)})
+      res.th.est <- lapply(n, function(n) 
+        th.est(xy, r = r, n, linear = linear))
       
       # Results of the selection criterium R squared
       res.r.squ <- sapply(1L:length(n), function(i) 
-	res.th.est[[i]][[1]][["r.squared"]])
+        res.th.est[[i]][[1]][["r.squared"]])
       
       # Result of the optimal regression
       xy.sum <- res.th.est[[which.max(res.r.squ)]]
       
       if (linear == FALSE) {
-	# Extract the coefficients of the regression.
-	a <- xy.sum[[1]][["coefficients"]][3,1]
-	b <- xy.sum[[1]][["coefficients"]][2,1]
-	c <- xy.sum[[1]][["coefficients"]][1,1]
-	
-	# Calculate the exact Ct value at the user defined fluorescence threshold.
-	# Use either the linear or quadratic model.
-	
-	sign <- inder(xy.sum[["values"]])
-	switch.sign <- which.max(sign[, "d1y"])
-	sqrt.delta <- sqrt(b^2 - 4*a*(c - r))
-	if (sign[switch.sign, "y"] < r) {
-	  x.cal <- (-b - sqrt.delta)/(2*a)
-	} else {
-	  x.cal <- (-b + sqrt.delta)/(2*a)
-	}
+        # Extract the coefficients of the regression.
+        a <- xy.sum[[1]][["coefficients"]][3, 1]
+        b <- xy.sum[[1]][["coefficients"]][2, 1]
+        c <- xy.sum[[1]][["coefficients"]][1, 1]
+        
+        # Calculate the exact Ct value at the user defined fluorescence threshold.
+        # Use either the linear or quadratic model.
+        
+        sign <- inder(xy.sum[["values"]])
+        switch.sign <- which.max(sign[, "d1y"])
+        sqrt.delta <- sqrt(b^2 - 4*a*(c - r))
+        if (sign[switch.sign, "y"] < r) {
+          x.cal <- (-b - sqrt.delta)/(2*a)
+        } else {
+          x.cal <- (-b + sqrt.delta)/(2*a)
+        }
       } else {
-	m <- xy.sum[[1]][["coefficients"]][1, 1]
-	n <- xy.sum[[1]][["coefficients"]][2, 1]
-	x.cal <- (r - m) / n
+        m <- xy.sum[[1]][["coefficients"]][1, 1]
+        n <- xy.sum[[1]][["coefficients"]][2, 1]
+        x.cal <- (r - m) / n
       }
       
       # Create the output fot the exact Ct value, the regression and the neighbours 
@@ -62,9 +62,9 @@ th.cyc <-
       colnames(res) <- c("cyc.th", "atFluo")
       
       new("th", .Data = res, 
-	  stats = xy.sum[["summary"]], 
-	  input = data.matrix(xy.sum[["values"]]))
-   }
+          stats = xy.sum[["summary"]], 
+          input = data.matrix(xy.sum[["values"]]))
+    }
   }
 
 
@@ -80,13 +80,13 @@ th.est <- function(xy, r, n, linear) {
   # Determine with a quadratic polynomial the equation for the neighbours of the 
   # cycle and fluorescence values at the threshold fluorescence.
   
-  if (linear == TRUE) {
-    xy.lm <- lm(xy.out[, 2] ~ xy.out[, 1])
+  xy.lm <- if (linear == TRUE) {
+    lm(xy.out[, 2] ~ xy.out[, 1])
   } else {
-    xy.lm <- lm(xy.out[, 2] ~ xy.out[, 1] + I(xy.out[, 1]^2))
+    lm(xy.out[, 2] ~ xy.out[, 1] + I(xy.out[, 1]^2))
   }
   
   
   # summary of statistical values of the fit
-  out <- list(summary = summary(xy.lm), values = xy.out)
+  list(summary = summary(xy.lm), values = xy.out)
 }
